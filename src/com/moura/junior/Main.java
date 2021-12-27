c10ea544c3d43a9f94a5a7a44a0b6e706008f77d
Junior Moura <geraldomartins10@gmail.com>
2021-12-27 18:34:46 -0300

    Programa que calcula a área de quadriláteros

---
+++ src/com/moura/junior/Main.java
@@ -0,0 +1,18 @@
+package com.moura.junior;
+
+/**
+ * Classe principal para execução do exercício da Aula02
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Quadrilatero.area(3);
+        Quadrilatero.area(3,5);
+        Quadrilatero.area(4,5,6);
+        Quadrilatero.area(5f,5f);
+    }
+}
